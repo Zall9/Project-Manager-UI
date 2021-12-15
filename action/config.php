b5911adc307d41f84653aad33aa0e2f892b8657c
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2021-12-15 10:40:32 +0100

    Update config.php

--- action/config.php
+++ action/config.php
@@ -1,8 +1,8 @@
 <?php
 class config{
     public const DB_HOST = 'localhost';
-    public const DB_USER = 'root';
-    public const DB_PASSWORD = 'root!';
+    public const DB_USER = 'youruser';
+    public const DB_PASSWORD = 'yourpass';
     public const DB_NAME = 'Gestion_UI';
     //tables
     public const TABLE_USER_STORIES = 'User_Stories';
